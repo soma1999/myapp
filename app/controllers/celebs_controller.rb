754060a293de31a6324bac8ed70a4380c870c099
soma1999 <so12ma131999@gmail.com>
2021-02-14 22:12:57 +0900

    有名人登録のコントローラーの作成

--- app/controllers/celebs_controller.rb
+++ app/controllers/celebs_controller.rb
@@ -1,8 +1,21 @@
 class CelebsController < ApplicationController
 
   def new
+    @celeb = Celeb.new
   end
 
   def create
+    @celeb = Celeb.new(celebs_parameter)
+    if @celeb.save
+      redirect_to  new_celeb_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def celebs_parameter
+    params.require(:celeb).permit(:name,:email,:password_digest,:description)
   end
 end
