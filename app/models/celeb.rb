72a81500f9180167914810b803924db08cce75cc
soma1999 <so12ma131999@gmail.com>
2021-02-14 22:12:00 +0900

    celebモデルのバリデーションの作成

--- app/models/celeb.rb
+++ app/models/celeb.rb
@@ -1,2 +1,15 @@
 class Celeb < ApplicationRecord
+
+  with_options presence: true, uniqueness: true do
+    validates :email
+    validates :name
+  end
+
+  vallidates  
+    :password,
+    format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i},
+    length: {minmum: 6 }
+
+  validates :description, presence: true
+
 end
