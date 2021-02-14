cec17375e4c006629d48b688515de778f3fb57ed
soma1999 <so12ma131999@gmail.com>
2021-02-14 17:46:47 +0900

    deviseの導入

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, :birthday, presence: true  
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: {with:/\A[ァ-ヶ]+\z/} do
+    validates :first_name_katakana
+    validates :last_name_katakana
+  end
+
+  # has_many :celebs, through: rooms
+  # has_many :rooms
+  # has_many :message_users
+
+end
