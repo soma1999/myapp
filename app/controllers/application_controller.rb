5567654f8bce8a8cfad2b29cb968643c390f3f3a
soma1999 <so12ma131999@gmail.com>
2021-02-15 19:34:38 +0900

    has_secure_passwordメソッドの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :current_celeb
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_katakana, :last_name_katakana, :birthday])
+  end
+
+  def current_celeb
+    if session[:user_id]
+    @current_celeb = Celeb.find_by(id: session[:user_id])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "homes#index" 
-  resources :celebs, only: [:new, :create, :show]
+  resources :celebs, only: [:new, :create, :show] do
+    collection do
+      get 'new_second'
+      post 'login'
+    end
+  end
+
+  # get '/login', to: 'celebs#new_second'
+  # post '/login', to: 'celebs#login'
 end
