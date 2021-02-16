73c43f0b792f26b50a808d8e6befa469dbb6c8c9
soma1999 <so12ma131999@gmail.com>
2021-02-16 11:57:26 +0900

    roomの作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,10 @@
 class RoomsController < ApplicationController
+
+  def find_or_create_by
+    room = Room.find_or_create_by(celeb_id: params[:celeb_id], user_id: current_user.id)
+    redirect_to room_path(room)
+  end
+
+  def show
+  end
 end
