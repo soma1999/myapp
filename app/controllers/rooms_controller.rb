06d56150a07e4e75265f14bee0e850fb014f136d
soma1999 <so12ma131999@gmail.com>
2021-02-19 18:09:36 +0900

    チャット画面の装飾

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,8 +6,19 @@ class RoomsController < ApplicationController
   end
 
   def show
-    @user_comment = UserComment.new
-    @celeb_comment = CelebComment.new
     @room = Room.find(params[:id])
+
+
+      @user_comment = UserComment.new
+      @celeb_comment = CelebComment.new     
+      @user_comments = UserComment.where(user_id: @room.user_id)
+      @celeb_comments = CelebComment.where(celeb_id: @room.celeb_id)
+      @comments = (@user_comments + @celeb_comments).order("created_at DESC")
+    # else
+    #   @celeb_comment = CelebComment.new      
+    #   @user_comments = UserComment.all(user_id: @room.user_id)
+    #   @celeb_comments = CelebComment.all(celeb_id: @room.celeb_id)
+    #   @comments = (@user_comments + @celeb_comments).order("created_at DESC")
+    # end
   end
 end
