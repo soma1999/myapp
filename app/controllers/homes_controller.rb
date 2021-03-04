54ee5bfd26476a83d88a3b152ece72ee9ec46561
soma1999 <so12ma131999@gmail.com>
2021-03-04 14:29:02 +0900

    ゲストログインの実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,20 @@ class HomesController < ApplicationController
   def index
     @celebs = Celeb.all.order("created_at DESC")
   end
+
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = '1a' + Faker::Internet.password(min_length: 6)
+      user.password_confirmation = user.password
+      user.nickname = "ゲスト"
+      user.last_name = "山田"
+      user.first_name = "太郎"
+      user.first_name_katakana = "ヤマダ"
+      user.last_name_katakana = "タロウ"
+      user.birthday = "1999-12-13"
+
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
 end
