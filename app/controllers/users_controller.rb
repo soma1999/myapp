56b2917cac5bf7f2408c7096b78c3901b6a99b88
soma1999 <so12ma131999@gmail.com>
2021-03-09 19:04:24 +0900

    device:usercontrollerの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,16 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
+class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
     @rooms = Room.where(user_id: current_user.id).order("created_at DESC")
     @likes = Like.where(user_id: current_user.id)
-  
+
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     card = Card.find_by(user_id: current_user.id)
-  
+
     redirect_to new_card_path and return unless card.present?
-  
+
     customer = Payjp::Customer.retrieve(card.customer_token)
     @card = customer.cards.first
   end
-  
-  def facebook
-    authorization
-  end
 
-  def google_oauth2
-    authorization
-  end
-
-  private
-
-  def authorization
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-  end
 end
