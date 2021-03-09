6ef1a5af31ff2cec99f8baab87e47df30c344833
soma1999 <so12ma131999@gmail.com>
2021-03-09 18:18:03 +0900

    sns_credentialsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_103952) do
+ActiveRecord::Schema.define(version: 2021_03_09_090451) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -105,6 +105,15 @@ ActiveRecord::Schema.define(version: 2021_03_03_103952) do
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -137,4 +146,5 @@ ActiveRecord::Schema.define(version: 2021_03_03_103952) do
   add_foreign_key "orders", "rooms"
   add_foreign_key "rooms", "celebs"
   add_foreign_key "rooms", "users"
+  add_foreign_key "sns_credentials", "users"
 end
