0f9046db8557ccdea97fda0cbd9f82d6171d9448
soma1999 <so12ma131999@gmail.com>
2021-03-11 17:09:03 +0900

    stanp_channelをmessage_channelと統合

--- app/channels/stanp_channel.rb
+++
@@ -1,9 +0,0 @@
-class StanpChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "stanp_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
