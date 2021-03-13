05733119c9c31cee499dbaf95c3f734a9d099831
soma1999 <so12ma131999@gmail.com>
2021-03-13 19:30:06 +0900

    blocks_controllerにアクセス制限

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,4 +1,5 @@
 class BlocksController < ApplicationController
+  before_action :ensure_current_user, only: [:create, :destroy]
 
   def create
     @block = Block.create!(room_id: params[:room_id])
@@ -9,6 +10,15 @@ class BlocksController < ApplicationController
     @block = Block.find(params[:id])
     @block.destroy
     redirect_to celeb_path(@current_celeb.id)
+  end
+
+  private
+
+  def ensure_current_user
+    @room = Room.find(params[:room_id])
+    unless @current_celeb.id == @room.celeb.id
+      redirect_to root_path
+    end
   end
 
 end
