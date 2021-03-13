77253430979109175da3e678894e66631530ba2f
soma1999 <so12ma131999@gmail.com>
2021-03-13 20:41:35 +0900

    userモデルのFactory.bot作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    email {Faker::Internet.free_email}
+    password = '1a' + Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    nickname {"Sunny太郎"}
+    first_name {"山田"}
+    last_name  {"太郎"}
+    first_name_katakana {"ヤマダ"}
+    last_name_katakana {"タロウ"}
+    birthday {"2020/01/01"} 
+  end
+end
