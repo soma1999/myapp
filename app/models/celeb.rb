0ebcc2f9c051f0a5d87800ec5f088310ffe91ba2
soma1999 <so12ma131999@gmail.com>
2021-03-15 12:11:31 +0900

    celbモデルのバリデーションの修正

--- app/models/celeb.rb
+++ app/models/celeb.rb
@@ -2,14 +2,25 @@ class Celeb < ApplicationRecord
   has_secure_password
   has_one_attached :image
 
-  with_options presence: true, uniqueness: true do
+  with_options presence: true do
+    validates :email
+    validates :name
+    validates :password
+    validates :image
+    validates :description
+  end
+
+  with_options uniqueness: true do
     validates :email
     validates :name
   end
 
-  validates  :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}, length: {minimum: 6 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
 
-  validates :description, presence: true, length: {maximum: 120}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}, length: {minimum: 6}
+
+  validates :description, length: {maximum: 120}
 
 
   has_many :users, through: :rooms
